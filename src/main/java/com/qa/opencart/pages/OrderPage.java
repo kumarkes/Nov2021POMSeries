a35b3a7855d5c2280d1c7d8d11d9235e501253fe
Keshav Kumar <kshvkmr2@gmail.com>
2022-02-19 10:02:52 +0530

    Order page is updated as per Review Comment

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,8 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
+	By order = By.id("Order");
+
 	public void order() {
-		System.out.println("Order is Created");
+		System.out.println("Order ID :12345");
 	}
 
 }
